4caee75c9dfdff2dfc2116ec5c2afa51839863af
Santiago Pardo Morales <s.pardom@uniandes.edu.co>
2025-04-20 01:44:27 +0000

    Finished Repositories

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,22 @@
 package com.example.repository;
 
-public interface AccountRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.entity.Account;
+
+//Repository injection
+@Repository
+public interface AccountRepository extends JpaRepository<Account,Integer> {
+    //Use JPQL
+    //JPQL Query to find account with username
+    @Query("SELECT account FROM Account account WHERE account.username = :user") 
+    Account findByUsername(@Param("user") String username);
+
+    //JPQL Query to find Account by username and password
+    @Query("SELECT account FROM Account account WHERE account.username = :user AND account.password = :pwd")
+    Account findByUsernameAndPassword(@Param("user") String username, @Param("pwd") String password);
+
 }
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,36 @@
 package com.example.repository;
 
-public interface MessageRepository {
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.entity.Message;
+
+//Repository injectionn
+@Repository
+public interface MessageRepository extends JpaRepository<Message,Integer> {
+
+    //Modifying and transactional injection
+    //JPQL Query to delete message by message id
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Message message WHERE message.id = :id")
+    int deleteMessageById(@Param("id") Integer id);
+
+    //Update message given its id
+    @Modifying
+    @Transactional
+    @Query("UPDATE Message message SET message.messageText = :text WHERE message.messageId = :id")
+    int updateMessage(@Param("text") String newMessage, @Param("id") Integer id);
+
+    //Find all messages given account id
+    @Query("SELECT message FROM Message message WHERE message.postedBy = :id")
+    List<Message> findMessagesByUserId(@Param("id") Integer id);
+
 }
