2cd2a2d00e3866b03913969fe39b06f77e3b6d3c
Santiago Pardo Morales <s.pardom@uniandes.edu.co>
2025-04-22 00:55:56 +0000

    All Test Passed

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -7,7 +7,9 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,25 +38,6 @@ public class SocialMediaController {
         this.messageService = messageService;
     }
 
-    //Endpoint that handles register action, using a POST Request
-    @PostMapping("/register")
-    public ResponseEntity register(@RequestBody Account account) {
-        return ResponseEntity.status(HttpStatus.valueOf(accountService.register(account))).build();
-    }
-
-    //Endpoint that handles login action, using a POST Request
-    @PostMapping("/login")
-    public ResponseEntity login(@RequestBody Account account) {
-        Account act_account = accountService.login(account);
-        if (act_account == null) {
-            //If account does not exists, throws unauthorized request
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        else{
-            //Throw request successful and use account
-            return ResponseEntity.status(HttpStatus.OK).body(act_account);
-        }
-    }
 
     //Endpoint to create a new message using POST Request
     @PostMapping("/messages")
@@ -72,11 +55,32 @@ public class SocialMediaController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
+        //Deleting Message By Id using DELETE Request
+    @DeleteMapping("/messages/{messageId}")
+    public ResponseEntity deleteMessageById(@PathVariable("messageId") Integer id) {
+        int result = messageService.deleteMessageById(id);
+
+        //If the result (rows deleted) is more than 0
+        if (result > 0) {
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+        }
+        //If no row was deleted, throw an OK Request with an empty message
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
     //Endpoint to return all messages using GET request
     @GetMapping("/messages")
     public ResponseEntity getAllMessages(){
         //Throws OK Request and executes method
         return ResponseEntity.status(HttpStatus.OK).body(messageService.getAllMessages());
+    }
+
+
+    //Get Messages By User ID, using GET Request
+    @GetMapping("/accounts/{accountId}/messages")
+    public ResponseEntity getMessagesByUserId(@PathVariable("accountId") Integer id){
+        //Throw OK Request and Execute Method from Service in Body
+        return ResponseEntity.status(HttpStatus.OK).body(messageService.getMessageByUserId(id));
     }
 
     @GetMapping("/messages/{messageId}")
@@ -91,6 +95,45 @@ public class SocialMediaController {
         //Returns OK request, but empty message
         return ResponseEntity.status(HttpStatus.OK).build();
     }
+
+
+    //Update a Message, using a PATCH request
+    @PatchMapping("/messages/{messageId}")
+    public ResponseEntity updateMessage(@RequestBody Message message,@PathVariable("messageId") Integer id){
+        int result = messageService.updateMessage(message.getMessageText(), id);
+
+        //If result (rows affected) is more than 0
+        if (result > 0) {
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+        }
+
+        //If message not found, throw a bad request
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+
+    //Endpoint that handles login action, using a POST Request
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody Account account) {
+        Account act_account = accountService.login(account);
+        if (act_account == null) {
+            //If account does not exists, throws unauthorized request
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        else{
+            //Throw request successful and use account
+            return ResponseEntity.status(HttpStatus.OK).body(act_account);
+        }
+    }
+
+
+    //Endpoint that handles register action, using a POST Request
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody Account account) {
+        return ResponseEntity.status(HttpStatus.valueOf(accountService.register(account))).build();
+    }
+
+    
 
 
 }
