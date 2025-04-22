0b4fb301397257925111c295bf0e1013b3e96cee
Santiago Pardo Morales <s.pardom@uniandes.edu.co>
2025-04-22 00:03:03 +0000

    Some More Tests Passed

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,15 +1,20 @@
 package com.example.controller;
 
+import java.util.Optional;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.Account;
+import com.example.entity.Message;
 import com.example.service.AccountService;
 import com.example.service.MessageService;
 
@@ -50,5 +55,42 @@ public class SocialMediaController {
             return ResponseEntity.status(HttpStatus.OK).body(act_account);
         }
     }
+
+    //Endpoint to create a new message using POST Request
+    @PostMapping("/messages")
+    public ResponseEntity createMessage(@RequestBody Message message) {
+
+        //Check if account exists
+        if (accountService.findByUserId(message.getPostedBy()).isPresent()) {
+            Message new_message = messageService.createMessage(message);
+            //Check if message created is not null, if it isn't, an OK Request is thrown
+            if (new_message != null) {
+                return ResponseEntity.status(HttpStatus.OK).body(new_message);
+            }
+        }
+        //Throws a bad request by default
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    //Endpoint to return all messages using GET request
+    @GetMapping("/messages")
+    public ResponseEntity getAllMessages(){
+        //Throws OK Request and executes method
+        return ResponseEntity.status(HttpStatus.OK).body(messageService.getAllMessages());
+    }
+
+    @GetMapping("/messages/{messageId}")
+    public ResponseEntity getMessageById(@PathVariable("messageId") Integer id){
+        Optional<Message> message = messageService.getMessageById(id);
+
+        //Checks if message retrieved exists and executes operation throwing successful message
+        if (message.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(message);
+        }
+
+        //Returns OK request, but empty message
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
 
 }
